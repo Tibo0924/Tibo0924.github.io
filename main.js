084708c81e9f57880ef5e74ee7a163e37eb1f541
Tibo <tiborkeri@icloud.com>
2020-01-29 10:02:13 +0000

    *-*

--- main.js
+++ main.js
@@ -1,9 +1,44 @@
-// Make sure sw are supported
+// Register service worker to control making site work offline
+
 if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("./sw_cached_site.js")
-      .then(reg => console.log("Service Worker: Registered (Pages)"))
-      .catch(err => console.log(`Service Worker: Error: ${err}`));
+  navigator.serviceWorker.register("pwa.js").then(function() {
+    console.log("Service Worker Registered wiii");
   });
+}
+
+// Code to handle install prompt on desktop
+
+let deferredPrompt;
+const addBtn = document.querySelector(".btn");
+addBtn.style.display = "none";
+
+window.addEventListener("beforeinstallprompt", e => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI to notify the user they can add to home screen
+  addBtn.style.display = "block";
+
+  addBtn.addEventListener("click", e => {
+    // hide our user interface that shows our A2HS button
+    addBtn.style.display = "none";
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then(choiceResult => {
+      if (choiceResult.outcome === "accepted") {
+        console.log("User accepted the A2HS prompt");
+      } else {
+        console.log("User dismissed the A2HS prompt");
+      }
+      deferredPrompt = null;
+    });
+  });
+});
+
+("standalone" in window.navigator && !window.navigator.standalone) ||
+  !window.matchMedia("(display-mode: standalone)").matches();
+{
+  addToHomeScreen();
 }
