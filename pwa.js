36e1009fee63b56aee102af9802ae1397519edbc
Tibo <tiborkeri@icloud.com>
2020-01-29 11:40:55 +0000

    rename

--- pwa.js
+++ pwa.js
@@ -1,6 +1,6 @@
 self.addEventListener("install", function(e) {
   e.waitUntil(
-    caches.open("video-store").then(function(cache) {
+    caches.open("offline").then(function(cache) {
       return cache.addAll(["/index.html"]);
     })
   );
